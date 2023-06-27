031288ebf96e04790544ab988737414fa7de5978
ARNAV <ARNAV@DESKTOP-310VSOT>
2023-06-27 22:40:41 +0100

    InitialCommit

---
+++ Spring-MVC/src/test/java/com/nav/mvc/SpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nav.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
